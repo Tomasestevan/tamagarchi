d9faafa5368a72f33b567d3acfd613216fd5df8c
Tomasestevan <tomasestevan1997@gmail.com>
2021-02-05 00:42:26 -0300

    ahi ta

--- src/tamagarchi/Main.java
+++ src/tamagarchi/Main.java
@@ -4,15 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 	Tamagochi wachin1;
-	wachin1 = new Tamagochi ("chito",5,false);
+	wachin1 = new Tamagochi ("chito",5,false, "contento",5);
 
 	Tamagochi wachin2;
-	wachin2 = new Tamagochi ("cores",3, true);
+	wachin2 = new Tamagochi ("cores",3, true, "triste",4);
 
-   public void jugeandos{
-       wachin1.jugarenconjunto();
-       wachin2.jugarenconjunto();
-        }
+	Tamagochi wachin3;
+	wachin3 = new Tamagochi ("semplo",6, false, "hambriento",2);
+
+
+
+
+
+
+
+
+   //public void jugeandos {
+      // wachin1.jugarenconjunto();
+       //wachin2.jugarenconjunto();
+        //}
 
 
 
--- src/tamagarchi/Tamagochi.java
+++ src/tamagarchi/Tamagochi.java
@@ -4,25 +4,48 @@ public class Tamagochi {
     public String Nombre;
     public int Felicidad;
     public boolean Covid;
+    public String Estado;
+    public int CuantasVecesJugo;
 
-    public Tamagochi (String nombre, int felicidad, boolean covid){
+    public Tamagochi (String nombre, int felicidad, boolean covid, String estado, int cuantasvecesjugo){
         Nombre = nombre;
         Felicidad = felicidad;
         Covid = covid;
+        Estado = estado;
+        CuantasVecesJugo = cuantasvecesjugo;
 
     }
-    public void come(){
-        Felicidad++;
-    }
-    public void juega(){
-        Felicidad = Felicidad + 2;
-    }
-    public void jugarenconjunto(){
-        Felicidad = Felicidad + 5;
-    }
-    public void leagarracovid(){
-        Covid = true;
+    public void come() {
+        if (Estado == "hambriento") {Estado = "contento"; }
+
+        if (Estado == "contento"){ Felicidad++;}
+
+        if (Estado == "triste" && Covid == false){Estado = "contento";}
     }
 
+    public void juega() {
+        if (Estado == "contento") {Felicidad = Felicidad + 2;CuantasVecesJugo++;
+        if (CuantasVecesJugo == 2){CuantasVecesJugo = 0;Estado = "hambriento";}}
 
-}
+        if (Estado == "triste") {Estado = "contento";}
+
+        if (Estado == "hambriento") {Felicidad = Felicidad - 4;Estado = "triste";}
+    }
+        public void jugarenconjunto(Tamagochi wach){
+            if (Estado == "hambriento") {return;}
+
+            if (Estado == "triste") {Estado = "contento";wach.Estado = "contento";}
+
+            if (Estado == "contento"){this.juega();wach.juega();wach.Felicidad = wach.Felicidad + 4;}
+
+        }
+
+    }
+    //public void leagarracovid(){
+          //  Covid = true;
+    // }
+
+
+//public void leagarracovid(){
+//  Covid = true;
+//}
